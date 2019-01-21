1ad736df47bd2a30a10b079fb33f47ac73ebac66
Nagahama Hokuto <e185751@ie.u-ryukyu.ac.jp>
2019-01-21 22:01:56 +0900

    3rd commit

--- src/main/jp/ac/uryukyu/ie/e185751/Main.java
+++ src/main/jp/ac/uryukyu/ie/e185751/Main.java
@@ -15,6 +15,48 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         Player p;
 
+        while (true){
+            if (whiteTurn){
+                p = wp;
+            }
+            else{
+                p = bp;
+            }
+            System.out.println(p.name + "のターンです");
+            ArrayList<Vector2> movablePoints = p.getMovablePoints(b);
+
+            if (movablePoints.isEmpty()){
+                whiteTurn = !whiteTurn;
+                endFlag += 1;
+                if (endFlag == 2){
+                    break;
+                }
+                continue;
+            }
+
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            Vector2 point = new Vector2(x,y);
+
+            if (!p.isMovable(movablePoints, point)){
+                System.out.println("そこには置けません\n");
+                continue;
+            }
+
+
+            p.move(b, point);
+
+            p.change(b,point);
+
+            b.display();
+
+            endFlag = 0;
+
+            whiteTurn = !whiteTurn;
+        }
+
+        System.out.println("ゲーム終了");
+
 
 
 
